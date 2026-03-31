5971358586df2996a027324e6ac609089bab0e52
TiteTiff <simonchautemps.tiffanie@gmail.com>
2026-03-31 16:40:17 +0200

    Deuxième cycle TDD terminé, conversion en chiffres romains de 1 à 3

--- DigitToRoman.py
+++ DigitToRoman.py
@@ -3,6 +3,6 @@ class DigitToRoman:
     roman:list = ["I", "V", "X", "L", "C", "D", "M"]
 
     def int_one_to_roman(number:int) ->str:
-        return DigitToRoman.roman[0]
+        return "I"*number
 
 
--- DigitToRoman_test.py
+++ DigitToRoman_test.py
@@ -3,6 +3,10 @@ import DigitToRoman
 
 class DigitToRoman_test(unittest.TestCase):
     def test_one(self):
-        self.assertEqual(DigitToRoman.DigitToRoman.int_one_to_roman(1), "I")  # add assertion here
+        self.assertEqual(DigitToRoman.DigitToRoman.int_one_to_roman(1), "I")
 
+    def test_two(self):
+        self.assertEqual(DigitToRoman.DigitToRoman.int_one_to_roman(2), "II")
 
+    def test_three(self):
+        self.assertEqual(DigitToRoman.DigitToRoman.int_one_to_roman(3), "III")
