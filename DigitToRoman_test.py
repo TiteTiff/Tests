de25846b55c02b16bbf324619c78f537ea1605a0
TiteTiff <simonchautemps.tiffanie@gmail.com>
2026-03-31 21:00:30 +0200

    Troisième cycle TDD en cours

--- DigitToRoman_test.py
+++ DigitToRoman_test.py
@@ -3,10 +3,13 @@ import DigitToRoman
 
 class DigitToRoman_test(unittest.TestCase):
     def test_one(self):
-        self.assertEqual(DigitToRoman.DigitToRoman.int_one_to_roman(1), "I")
+        self.assertEqual(DigitToRoman.DigitToRoman.int_to_roman(1), "I")
 
     def test_two(self):
-        self.assertEqual(DigitToRoman.DigitToRoman.int_one_to_roman(2), "II")
+        self.assertEqual(DigitToRoman.DigitToRoman.int_to_roman(2), "II")
 
     def test_three(self):
-        self.assertEqual(DigitToRoman.DigitToRoman.int_one_to_roman(3), "III")
+        self.assertEqual(DigitToRoman.DigitToRoman.int_to_roman(3), "III")
+
+    def test_four(self):
+        self.assertEqual(DigitToRoman.DigitToRoman.int_to_roman(4), "IV")
