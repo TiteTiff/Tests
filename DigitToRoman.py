7b2b76049ba097878ef86ba98a38c5460b979dee
TiteTiff <simonchautemps.tiffanie@gmail.com>
2026-03-31 15:21:06 +0200

    Premier cycle de TDD fait, 1 convertit en I

---
+++ DigitToRoman.py
@@ -0,0 +1,8 @@
+
+class DigitToRoman:
+    roman:list = ["I", "V", "X", "L", "C", "D", "M"]
+
+    def int_one_to_roman(number:int) ->str:
+        return DigitToRoman.roman[0]
+
+
--- DigitToRoman_test.py
+++ DigitToRoman_test.py
@@ -1,7 +1,8 @@
 import unittest
+import DigitToRoman
 
-class MyTestCase(unittest.TestCase):
+class DigitToRoman_test(unittest.TestCase):
     def test_one(self):
-        self.assertEqual(True)  # add assertion here
+        self.assertEqual(DigitToRoman.DigitToRoman.int_one_to_roman(1), "I")  # add assertion here
 
 
